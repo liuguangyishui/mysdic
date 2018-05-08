46d5387a0a62061c9343460db2045236b183d3a3
hyl <mysdic@liuguangyishui.com>
2018-05-08 18:55:12 +0800

    5/8/6

--- lbdexsdic/SDICSEInstrInfo.cpp
+++ lbdexsdic/SDICSEInstrInfo.cpp
@@ -63,22 +63,22 @@ void SDICSEInstrInfo::adjustStackPtr(unsigned SP, int64_t Amount,
 				     MachineBasicBlock &MBB,
 				     MachineBasicBlock::iterator I) const {
   DebugLoc DL = I != MBB.end() ? I->getDebugLoc() : DebugLoc();
-  unsigned ADDiua = SDIC::ADDiua;
+  // unsigned ADDiua = SDIC::ADDiua;
   unsigned ADDu = SDIC::ADDu;
   unsigned ADDiu = SDIC::ADDiu;
   if (isInt<16>(Amount)) {
     printf("\nHYLHYL: isInt<16>(Amount)hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n");
     // addiu sp, sp, amount
     //My modify
-    // BuildMI(MBB, I, DL, get(ADDiu), SP).addReg(SP).addImm(Amount);
-    BuildMI(MBB, I, DL, get(ADDiua), SP).addReg(SP).addImm(1).addImm(1);
+     BuildMI(MBB, I, DL, get(ADDiu), SP).addReg(SP).addImm(Amount);
+     // BuildMI(MBB, I, DL, get(ADDiua), SP).addReg(SP).addImm(1).addImm(1);
 
     uint64_t i=4;//both call fun and return fun should operate the PC register
     if(Amount<0)
       i=-i;
     //MY modify
-    //BuildMI(MBB,I,DL,get(ADDiu),SDIC::PCL).addReg(SDIC::PCL).addImm(i);
-      BuildMI(MBB, I, DL, get(ADDiua), SDIC::PCL).addReg(SDIC::PCL).addImm(1).addImm(1);
+    BuildMI(MBB,I,DL,get(ADDiu),SDIC::PCL).addReg(SDIC::PCL).addImm(i);
+    //  BuildMI(MBB, I, DL, get(ADDiua), SDIC::PCL).addReg(SDIC::PCL).addImm(1).addImm(1);
       BuildMI(MBB,I,DL,get(SDIC::MOVF),SDIC::WREG).addReg(SDIC::PCL).addImm(i);
 
   }

aa4da661c381e51cc51c388ed99369dbde5d840e
hyl <mysdic@liuguangyishui.com>
2018-05-31 08:47:42 +0800

    5/30

--- lbdexsdic/SDICISelLowering.cpp
+++ lbdexsdic/SDICISelLowering.cpp
@@ -150,7 +150,7 @@ SDValue SDICTargetLowering::LowerSTORE(SDValue Op, SelectionDAG &DAG) const
     SDValue Op2 = Op.getOperand(2);
     SDValue Op3 = Op.getOperand(3);
   printf("this is a test");
-  return DAG.getNode(SDICISD::Pesuo, dl, MVT::i32, Op0, Op1);//, Op2, Op3);
+  return DAG.getNode(SDICISD::Pesuo, dl, MVT::i32, Op1);//, Op2, Op3);
    // return DAG.getNode(SDICISD::Pesuo, dl, MVT::i32);
 
 }

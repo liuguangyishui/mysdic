4da599b91b3f4c1e4c31ff93cf28b7dc0a1df87f
hyl <mysdic@liuguangyishui.com>
2018-06-04 19:32:28 +0800

    5/5

--- lbdexsdic/SDICISelLowering.cpp
+++ lbdexsdic/SDICISelLowering.cpp
@@ -307,7 +307,7 @@ SDICTargetLowering::LowerReturn(SDValue Chain,
     Flag = Chain.getValue(1);
     RetOps.push_back(DAG.getRegister(V0, getPointerTy(DAG.getDataLayout())));
     }*/
-  SDValue Op0 = Chain.getOperand(0);
+  SDValue Op0 = Chain.getOperand(1);
   //@Ordinary struct type: 2 }
  printf("\n'''''''''''''''''''''''''''''''''33333333333");
   RetOps[0] = Chain;

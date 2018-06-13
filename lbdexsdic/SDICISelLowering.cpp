78a56c794aba9e9b4f5420549a1935b9117ee572
hyl <mysdic@liuguangyishui.com>
2018-06-13 18:35:09 +0800

    5/5

--- lbdexsdic/SDICISelLowering.cpp
+++ lbdexsdic/SDICISelLowering.cpp
@@ -112,7 +112,7 @@ SDValue SDICTargetLowering::LowerOperation(SDValue Op, SelectionDAG &DAG) const
     case ISD::MUL:   return LowerMUL(Op, DAG);
 
     case ISD::SUB:   return LowerSUB(Op, DAG);
-      // case ISD::LOAD:  return LowerLOAD(Op, DAG);
+    case ISD::LOAD:  return LowerLOAD(Op, DAG);
 
       
       case ISD::STORE: return LowerSTORE(Op, DAG);

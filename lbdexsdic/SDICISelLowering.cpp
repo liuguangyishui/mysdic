904dc89462c0458ae6883c1d9ed78a2c299c37d6
hyl <mysdic@liuguangyishui.com>
2018-06-20 14:19:16 +0800

    4“ git commit -m 4“

--- lbdexsdic/SDICISelLowering.cpp
+++ lbdexsdic/SDICISelLowering.cpp
@@ -249,7 +249,7 @@ SDValue SDICTargetLowering::LowerSTORE(SDValue Op, SelectionDAG &DAG) const
       printf("\n this is not voidTy");
       //   SDValue Flag0 = DAG.getNode(SDICISD::Pesuo, dl, MVT::i32, Op0);//R
        //return DAG.getNode(SDICISD::Pesuo, dl, MVT::Other, Op1);//, Op2, Op3);
-      SDValue Flag0 = DAG.getNode(ISDD::EntryToken, dl, MVT::Other);
+      SDValue Flag0 = DAG.getNode(ISD::EntryToken, dl, MVT::Other);
       return DAG.getNode(SDICISD::Addwfrr, dl, MVT::Other, Op1, Flag0);//,  Flag0);//C + R
      
     }

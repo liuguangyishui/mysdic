3dcc5394d8d43eea9670973bad0a34a4a2ca910e
hyl <mysdic@liuguangyishui.com>
2018-06-30 15:14:34 +0800

    6/30

--- lbdexsdic/SDICISelLowering.cpp
+++ lbdexsdic/SDICISelLowering.cpp
@@ -133,7 +133,7 @@ SDValue SDICTargetLowering::LowerOperation(SDValue Op, SelectionDAG &DAG) const
     case ISD::SDIV:   return LowerDIV(Op, DAG);
 
     case ISD::SUB:   return LowerSUB(Op, DAG);
-      // case ISD::LOAD:  return LowerLOAD(Op, DAG);
+    case ISD::LOAD:  return LowerLOAD(Op, DAG);
 
       
       //  case ISD::STORE: return LowerSTORE(Op, DAG);
@@ -277,11 +277,25 @@ SDValue SDICTargetLowering::LowerLOAD(SDValue Op, SelectionDAG &DAG) const
 {
   SDLoc dl(Op);
 
+  LoadSDNode *LD = cast<LoadSDNode>(Op);
+  SDValue Chain = LD->getChain();
+  SDValue BasePtr = LD->getBasePtr();
+
+  switch (BasePtr.getOpcode())
+    {
+    default: printf("this is defualt");
+      break;
+    case ISD::FrameIndex:
+      printf("\n this is FrameIndex");
+      return SDVlaue();
+  
   SDValue Op0 = Op.getOperand(0);
   SDValue Op1 = Op.getOperand(1);
   SDValue Op2 = Op.getOperand(2);
   SDValue Op3 = Op.getOperand(3);
 
+  
+  
   printf("\n the Op0 is %u",Op0);
   printf("\n%u",Op1);
   printf("\n%u\n",Op2);

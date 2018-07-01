492d783b7cec4bc7b96408c53e250c8e27afe547
hyl <mysdic@liuguangyishui.com>
2018-07-01 15:35:00 +0800

    7/1

--- lbdexsdic/InstPrinter/SDICInstPrinter.cpp
+++ lbdexsdic/InstPrinter/SDICInstPrinter.cpp
@@ -80,7 +80,7 @@ void SDICInstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
     }
     }
   //add for the load node
-  if(getOpcodeName(MI->getOpcode())== "ld")
+  if(getOpcodeName(MI->getOpcode())== "load")
     {
       if(Op.isReg())
 	{

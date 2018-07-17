3ed3b019298c6aec5d580cedc3fbc81ddbd5fed4
hyl <mysdic@liuguangyishui.com>
2018-07-17 18:58:32 +0800

    8/8

--- lbdexsdic/SDICSEInstrInfo.cpp
+++ lbdexsdic/SDICSEInstrInfo.cpp
@@ -154,9 +154,9 @@ void SDICSEInstrInfo::adjustStackPtr(unsigned SP, int64_t Amount,
     // BuildMI(MBB, I, DL, get(ADDLW)).addImm(1);
     BuildMI(MBB, I, DL, get(Movwf)).addImm(1).addReg(STKPTR);
     BuildMI(MBB, I, DL, get(Movf)).addImm(1).addReg(PCL);
-    BuildMI(MBB, I, DL, get(Movwf)).addImm(1).addReg(TOSL);
-    BuildMI(MBB, I, DL, get(Movf)).addImm(1).addReg(PCH);
-    BuildMI(MBB, I, DL, get(Movwf)).addImm(1).addReg(TOSH);
+    //BuildMI(MBB, I, DL, get(Movwf)).addImm(1).addReg(TOSL);
+    // BuildMI(MBB, I, DL, get(Movf)).addImm(1).addReg(PCH);
+    //BuildMI(MBB, I, DL, get(Movwf)).addImm(1).addReg(TOSH);
     
     
     // BuildMI(MBB, I, DL, get(ADDiu), SP).addReg(SP).addImm(Amount);

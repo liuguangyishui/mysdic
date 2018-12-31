8fb4be1ab3d28349cedec60531034ce50e5e2d3b
HYL <hyl@localhost.localdomain>
2018-12-31 15:30:28 +0800

    2018/12/31

--- lbdexsdic/SDICISelDAGToDAG.cpp
+++ lbdexsdic/SDICISelDAGToDAG.cpp
@@ -186,7 +186,7 @@ void SDICDAGToDAGISel::Select(SDNode *Node) {
     // ReplaceNode(Node, NewNode);
   
 
-    ReplaceUses(SDValue(ST, 1), SDValue(NewNode, 0));
+    ReplaceUses(SDValue(ST, 0), SDValue(NewNode, 1));
     //   CurDAG->SelectNodeTo(Node, SDICISD::Addwf, MVT::i32);
   }
   case ISD::GLOBAL_OFFSET_TABLE:
